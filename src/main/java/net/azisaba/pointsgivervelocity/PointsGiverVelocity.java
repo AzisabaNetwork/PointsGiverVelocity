2ca8193d8403e6f01e47b5e88c128eaf37ad6718
siloneco <siloneco4646@gmail.com>
2022-08-22 07:51:03 +0900

    :bookmark: bump version to 1.0.0

--- src/main/java/net/azisaba/pointsgivervelocity/PointsGiverVelocity.java
+++ src/main/java/net/azisaba/pointsgivervelocity/PointsGiverVelocity.java
@@ -19,7 +19,7 @@ import net.azisaba.pointsgivervelocity.sql.SQLConnector;
 @Plugin(
     id = "points-giver-velocity",
     name = "PointsGiverVelocity",
-    version = "0.0.1-SNAPSHOT",
+    version = "1.0.0",
     url = "https://github.com/AzisabaNetwork/PointsGiverVelocity",
     description = "PointsGiver for Velocity",
     authors = {"Azisaba Network"})
